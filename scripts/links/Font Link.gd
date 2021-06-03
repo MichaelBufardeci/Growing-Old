687c41f4380d3efcfe90d4425a14f61d79a34a42
MichaelBufardeci <michael.bufardeci@gmail.com>
2021-06-03 12:32:58 -0400

    organizing links

--- scripts/links/Font Link.gd
+++ scripts/links/Font Link.gd
@@ -3,4 +3,4 @@ extends Label
 func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.doubleclick:
-			OS.shell_open("https://ontspace.com/heaven-castro")
+			OS.shell_open("https://fontspace.com/heaven-castro")
